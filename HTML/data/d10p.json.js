ea4045f457b82a7a9b98754727462fcac5d9cce0
acamlibe <acamlibe@uncc.edu>
2021-11-18 19:01:30 -0500

    Added Data Dictionary HTML files

---
+++ HTML/data/d10p.json.js
@@ -0,0 +1,1 @@
+window.repositoryObject = {"title_prefix":null,"custom_fields":[],"procedures":[{"id":"p364","name":"add_person","type":"PROCEDURE","subtype":"PROCEDURE","is_user_defined":false,"custom_fields":{"Sample field: Status":{"value":null,"type":"LIST_CLOSED"}}}],"object_id":"d10p"};
