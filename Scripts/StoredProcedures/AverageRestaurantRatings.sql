c2c7d1278dd8018f81518a193f8b33e0987725b6
acamlibe <acamlibe@uncc.edu>
2021-11-27 21:08:21 -0500

    Added an optional parameter to allow filtering off restaurant ID.

--- Scripts/StoredProcedures/AverageRestaurantRatings.sql
+++ Scripts/StoredProcedures/AverageRestaurantRatings.sql
@@ -1,13 +1,34 @@
+/*
+    This stored procedure provides average restaurant ratings.
+    
+    You can provide a restaurant_id to filter the ratings for, 
+    or pass in NULL for an average rating for ALL restaurants.
+    
+*/
+
 USE `Campus_Eats_Fall2020`;
 DROP procedure IF EXISTS `average_restaurant_ratings`;
 
 DELIMITER $$
-CREATE PROCEDURE `average_restaurant_ratings` ()
+CREATE PROCEDURE `average_restaurant_ratings` (
+    IN search_restaurant_id INT
+)
+
 BEGIN
-    SELECT AVG (RATING)
-    FROM RestaurantRatingTable;
+    IF search_restaurant_id IS NULL THEN
+        SELECT AVG (RATING)
+        FROM RestaurantRatingTable;
+    ELSE
+        SELECT AVG (RATING)
+        FROM RestaurantRatingTable
+        WHERE restaurant_id = search_restaurant_id;
+    END IF;
 END$$
 
 DELIMITER ;
 
-CALL average_restaurant_ratings;
+# Call for average rating for ALL restaurants.
+CALL average_restaurant_ratings (NULL);
+
+# Call for average rating for only the restaurant with an ID of 3.
+CALL average_restaurant_ratings (3);
