8a96bd506d82c6c77aabd330e47da0869d83f17e
acamlibe <acamlibe@uncc.edu>
2021-11-27 20:49:57 -0500

    Created AverageRestaurantRatings stored procedure sql

---
+++ Scripts/StoredProcedures/AverageRestaurantRatings.sql
@@ -0,0 +1,14 @@
+USE `Campus_Eats_Fall2020`;
+DROP procedure IF EXISTS `average_restaurant_ratings`;
+
+DELIMITER $$
+USE `Campus_Eats_Fall2020`$$
+CREATE PROCEDURE `average_restaurant_ratings` ()
+BEGIN
+	SELECT AVG (RATING)
+    FROM RestaurantRatingTable;
+END$$
+
+DELIMITER ;
+
+CALL average_restaurant_ratings;
