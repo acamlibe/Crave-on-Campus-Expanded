86f22177eee6e87c6be9c09faaf5e7c431752ff8
Shina Dhingra <shina.dhingra11@gmail.com>
2021-12-03 12:25:36 -0500

    Stored Procedure for Avg Driver Ratings
    This is the 5th query from Deliverable 2.5

---
+++ Scripts/StoredProcedures/AverageDriverRatings.sql
@@ -0,0 +1,32 @@
+/*
+    This stored procedure provides average ratings for Driver given the Driver ID.
+    
+    
+*/
+
+USE `Campus_Eats_Fall2020`;
+DROP procedure IF EXISTS `average_driver_ratings`;
+
+DELIMITER $$
+CREATE PROCEDURE `average_driver_ratings` (
+    IN search_driver_id INT
+)
+
+BEGIN
+    IF search_driver_id IS NULL THEN
+        SELECT AVG (Rating)
+        FROM DriverRatingTable;
+    ELSE
+        SELECT AVG (Rating)
+        FROM DriverRatingTable
+        WHERE Driver_ID = search_driver_id;
+    END IF;
+END$$
+
+DELIMITER ;
+
+# Call for average rating for ALL Drivers.
+CALL average_driver_ratings (NULL);
+
+# Call for average rating for a given Driver ID.
+CALL average_driver_ratings(1);
