46ab5f42e74363cbba97eed5f40ab0865a211085
lmedinen <94883997+lmedinen@users.noreply.github.com>
2021-12-03 20:23:13 -0600

    Insert resords in restaurant table

---
+++ Scripts/Insert_Res_2.5.sql.sql
@@ -0,0 +1,7 @@
+INSERT INTO restaurant (restaurant_id, location, restaurant_name,schedule,website)
+VALUES ('column_Value 1' , "column_Value 2",'column_Value 3','column_Value 4', 'column_value 5');
+
+Example: 
+
+INSERT INTO restaurant (restaurant_id, location, restaurant_name,schedule,website)
+VALUES ('101' , "13240 coppermill drive,VA 20171-2011",'honeywell inc','10AM - 10PM', 'https://honeywell.com');
