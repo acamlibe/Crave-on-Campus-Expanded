ab21b737cbc2a6f30e197dda6eb45bbf16fcfbc4
acamlibe <acamlibe@uncc.edu>
2021-12-06 11:01:31 -0500

    Added add_order procedure to setup script.

--- Scripts/CampusEatsSetupScript.sql
+++ Scripts/CampusEatsSetupScript.sql
@@ -759,6 +759,36 @@ END$$
 
 DELIMITER ;
 
+DROP PROCEDURE IF EXISTS `add_order`;
+
+DELIMITER $$
+
+/* Create the Procedure */
+CREATE PROCEDURE `add_order` 
+(
+    IN added_person_id INT,
+    IN added_delivery_id INT,
+    IN added_location_id INT,
+    IN added_driver_id INT,
+    IN added_restaurant_id INT,
+    IN added_total_price FLOAT,
+    IN added_delivery_charge FLOAT
+)
+BEGIN
+	INSERT INTO `order` (person_id, delivery_id, location_id, driver_id, restaurant_id, total_price, delivery_charge) 
+	VALUES (
+		added_person_id, 
+		added_delivery_id, 
+		added_location_id, 
+		added_driver_id, 
+		added_restaurant_id, 
+		added_total_price, 
+		added_delivery_charge
+    );
+END$$
+
+DELIMITER ;
+
 /* Added Functions */
 DROP FUNCTION IF EXISTS `get_restaurant_id`;
 
